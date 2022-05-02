a085d806bf0c425a6cb2a95d3494bbb75eb268a1
grazhdanochka <mariyashakuro@gmail.com>
2022-05-02 21:12:19 +0300

    Logout, home, sign up where added

--- TodoWoo/urls.py
+++ TodoWoo/urls.py
@@ -21,6 +21,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     # Auth
-    path('signup/', views.signupuser, name='signupuser')
+    path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
     # Todos
+    path('current/', views.currenttodos, name='currenttodos'),
+    path('', views.home, name='home'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login, logout
+
+
+def home(request):
+    return render(request, 'todo/home.html')
 
 
 def signupuser(request):
@@ -8,7 +14,25 @@ def signupuser(request):
         return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html',
+                              {'form': UserCreationForm(),
+                               'error': 'That username has already been taken. Please choose another one'})
         else:
-            print('Hello')
+            return render(request, 'todo/signupuser.html',
+                          {'form': UserCreationForm(), 'error': 'Passwords did not match'})
+
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
+
+
+def logoutuser(request):
+    if request.method == 'POST':
+        logout(request)
+        return redirect('home')
